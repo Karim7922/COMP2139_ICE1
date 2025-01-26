44988b719f41449e40427d91e34c8bcc2e8dc443
Karim <101488793@georgebrown.ca>
2025-01-25 21:31:25 -0500

    commit

---
+++ Controllers/ProjectController.cs
@@ -0,0 +1,41 @@
+﻿using COMP2139_ICE.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP2139_ICE.Controllers;
+
+public class ProjectController: Controller
+{
+
+    public IActionResult Index()
+    {
+        var projects = new List<Project>()
+        {
+            new Project { ProjectId = 1, Name = "Project1", Description = "First Project" }
+        };
+        return View(projects);
+    }
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+        
+    }
+
+    [HttpPost]
+    public IActionResult Create(Project project)
+    {
+        return RedirectToAction("Index");
+    }
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Details(int id)
+    {
+        var project = new Project{ProjectId = id, Name = "Project 1", Description = "First Project"};
+        return View(project);
+    }
+    
+}
---
+++ Models/Project.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP2139_ICE.Models;
+
+public class Project
+{
+    /// <summary>
+    /// This is the primary key for projects
+    /// </summary>
+    public int ProjectId { get; set; }
+    
+    /// <summary>
+    /// The Name of the project
+    /// [Required]: Ensures this properly must be set must have a project name
+    /// </summary>
+    
+    [Required]
+    public required string Name { get; set; }
+    
+    public string Description { get; set; }
+    
+    [DataType(DataType.Date)]
+    public DateTime StartDate { get; set; }
+    
+    
+    [DataType(DataType.Date)]
+    public DateTime EndDate { get; set; }
+    
+    public string Status { get; set; }
+}
